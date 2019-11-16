99028e594f939f5d69d4e63246fb61cacf02d226
dennis-yin <dennis.yin26@gmail.com>
2019-11-16 00:21:45 +0000

    add seeds

--- db/schema/schema.sql
+++ db/schema/schema.sql
@@ -10,8 +10,8 @@ DROP TABLE IF EXISTS ratings CASCADE;
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL
-  password VARCHAR(500) NOT NULL
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(500) NOT NULL,
   profile_picture VARCHAR(2000)  -- Have a default profile picture
 );
 
@@ -23,16 +23,16 @@ CREATE TABLE categories (
 CREATE table pins (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  image VARCHAR(2000),
+  image VARCHAR(2000), -- Have a default profile picture
   title VARCHAR(255),
   description TEXT,
-  url VARCHAR(2000))
+  url VARCHAR(2000)
 );
 
 CREATE TABLE categories_pins (
   id SERIAL PRIMARY KEY NOT NULL,
   category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
-  pin_id INTEGER REFERENCES pins(id) ON DELETE CASCADE,
+  pin_id INTEGER REFERENCES pins(id) ON DELETE CASCADE
 );
 
 CREATE TABLE boards (
@@ -43,7 +43,7 @@ CREATE TABLE boards (
 CREATE TABLE boards_pins (
   id SERIAL PRIMARY KEY NOT NULL,
   board_id INTEGER REFERENCES boards(id) ON DELETE CASCADE,
-  pin_id INTEGER REFERENCES pins(id) ON DELETE CASCADE,
+  pin_id INTEGER REFERENCES pins(id) ON DELETE CASCADE
 );
 
 CREATE TABLE comments (
