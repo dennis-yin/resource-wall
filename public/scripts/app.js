b1ebcf8e298c9d722a9b37d8d3f4b25eb316a775
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2019-11-16 20:16:25 +0000

    sassing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,26 @@
+const createTweetElement = (tweetData) => {
+  const $tweet = $("<article>").addClass("tweet");
+  const safeHtml = escape(tweetData.content.text);
+  const timeStamp = moment(tweetData.created_at).fromNow();
+  const markup = `
+        <header>
+          <img class="avatar" src="${tweetData.user.avatars}">
+          <div class="userName">${tweetData.user.name}</div>
+          <div class="userId">${tweetData.user.handle}</div>
+        </header>
+        <div class="content">${safeHtml}</div>
+        <footer>
+          <div class="life">${timeStamp}</div>
+          <div class="shortcuts">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+          </div></footer>
+        `;
+  $($tweet).append(markup);
+  return $tweet;
+};
+
 $(() => {
   $.ajax({
     method: "GET",
@@ -6,5 +29,5 @@ $(() => {
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
-  });;
+  });
 });
