e06de95834a4bb2467a2eb7fb1b81f9a7ab3bc0a
Dennis Yin <dennis.yin26@gmail.com>
2019-11-20 23:53:06 +0000

    fix nav for real

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -6,22 +6,22 @@ const loadNav = () => {
   .done((data) => {
     if (data) {
       const markup = `
-      <p> ${data.user.name}</p>
-      <form class="myUser" method="GET" action="/user">
-        <button  type="home-button" class="home-button btn btn-light ml-auto">My  Resources</button>
+      <p class="loggedIn username"> ${data.user.name}</p>
+      <form class="loggedIn" method="GET" action="/user">
+        <button  type="home-button" class="home-button btn btn-light ml-auto">My Resources</button>
       </form>
-      <form class="myUser" method="POST" action="/data/logout">
+      <form class="loggedIn" method="POST" action="/data/logout">
         <button type="logout-button" class="logout-button btn btn-light">Logout</button>
       </form>
       `
       $("#navbar").append(markup)
     } else {
       const markup = `
-      <form class="myUser" method="GET" action="/login">
+      <form class="noUser" method="GET" action="/login">
         <button  type="home-button" class="home-button btn btn-light ml-auto">Login</button>
       </form>
-      <form class="myUser" method="GET" action="/register">
-        <button type="logout-button" class="logout-button btn btn-light">Register</button>
+      <form class="noUser" method="GET" action="/register">
+        <button type="register-button" class="register-button btn btn-light">Register</button>
       </form>
       `
       $("#navbar").append(markup)
