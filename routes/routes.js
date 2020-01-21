544bca81bdf5708e3f6935c019af7884777ea396
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-01-21 23:09:10 +0000

    added readme file with screenshots

--- routes/routes.js
+++ routes/routes.js
@@ -8,8 +8,6 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const cookieSession = require('cookie-session');
-const SALT_ROUNDS = 12;
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
